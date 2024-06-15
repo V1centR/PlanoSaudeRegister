347b08d6a22b895b2328d19ec711d1e40513b910
V1centR <vicentcdb@gmail.com>
2024-06-15 17:41:23 -0300

    First commit

---
+++ src/test/java/com/planosaude/com/PlanoSaudeRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planosaude.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoSaudeRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
